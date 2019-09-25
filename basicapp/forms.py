55a312bd0a898e6b2f14e3be3aa5583545e3540d
NuthanChandra <nuthanchandra1997@gmail.com>
2019-09-25 20:33:23 +0530

    Custom validator

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from django.core import validators
 
+def start_with_n(value):
+    if value[0].lower() != 'n':
+        raise forms.ValidationError("NAME NEEDS TO START WITH N")
+
 class FormName(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[start_with_n])
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
 
