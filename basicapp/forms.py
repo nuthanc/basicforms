23f4dd35797c55a13f8adf9f9a35bd7ec20886eb
NuthanChandra <nuthanchandra1997@gmail.com>
2019-09-25 20:48:15 +0530

    Custom validation for all fields at once

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from django.core import validators
 
-def start_with_n(value):
-    if value[0].lower() != 'n':
-        raise forms.ValidationError("NAME NEEDS TO START WITH N")
 
 class FormName(forms.Form):
-    name = forms.CharField(validators=[start_with_n])
+    name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
 
-    botCatcher = forms.CharField(required=False,
-     widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
-
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+        if email != vmail:
+            raise forms.ValidationError("EMAIL HAS TO BE THE SAME")
